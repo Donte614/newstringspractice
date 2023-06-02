dba15feea81b1c7cdc82adf79aaeaed832935259
donte614 <laffytaffy641@icloud.com>
2023-06-02 15:05:03 -0400

    if statement

--- index.js
+++ index.js
@@ -1,2 +1,83 @@
-let a =25
-console.log(a);
+// let a =25
+// console.log(a);
+
+
+
+// let age = 17;
+// if (age > 18){
+//     //do this
+//     console.log("access granted");
+
+// }
+// else{
+//     //do this
+//     console.log("access denied");
+// }
+
+
+
+
+// }
+// let username= "admin";
+// let password= "Pass614";
+
+// if (username=== "admin" ,  password==="pass614") {
+
+
+//     console.log( "access granted");}
+
+// //    else {
+// //     console.log("access denied");
+//    }
+
+// let wood=20;
+// if (wood>=40) {
+//     // we have enough for a treehouse
+//     console.log( "you can build a treehouse")
+// }
+// else if (wood >=20){
+//     //to bad 
+//     console.log("build stoole");
+// }
+// else{
+//     console.log("you are s.o.l");
+// }
+
+// let num =99;
+
+// if(num >=0 && num >=100){
+//     if(num !== 100){
+//         console.log( "number is positive and greater than 100");
+//     }else{
+//             console.log( "number is zero and not greater than 100" );
+//         }
+//     }
+
+//      else if (num>=0 && num < 100){
+//     if(num !==0)
+    
+//         console.log( "number is positive but not greater that 100");
+//         else{
+//             console.log( "number is zero and not greater than 100");
+//         }
+//         //console.log( "number is positiveor zero but not greater than 100");
+//         console.log("negative number");
+//     }
+ 
+let grade = 100;
+
+if(grade>= 80){
+    console. log( "you did a good job");
+}
+else if(grade >= 70){
+    console.log("keep trying");
+}
+else if(grade >= 60){
+    console.log("ehhhhh");
+}
+else if(grade >= 55){
+    console.log("not to good");
+}
+else if(grade >= 55){
+    console.log("bad grade but you'll get it");
+}
